4c9ec877edddd1955e038354dd5db9cdfbe99c92
Amir <hadizadeh1380@gmail.com>
2023-10-07 01:55:00 -0700

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Ganjoor Test',
+  description: 'It is a test based on ganjoor.net',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" dir="rtl">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
