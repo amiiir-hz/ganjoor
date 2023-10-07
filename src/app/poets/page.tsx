91a686488a6975fdb53e97cdbcd27cc1e6d1089d
Amir <hadizadeh1380@gmail.com>
2023-10-07 16:36:42 +0330

    last

--- src/app/poets/page.tsx
+++ src/app/poets/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useReactTable } from "@tanstack/react-table";
 import Image from "next/image";
 import { useQuery } from "react-query";
 
@@ -7,10 +8,73 @@ export default function Poets() {
     fetch("https://api.ganjoor.net/api/ganjoor/poets").then((res) => res.json())
   );
 
+  const table = useReactTable({
+    data,
+    columns: COLOUMN,
+  });
+
   // console.log(data)
   return (
     <main className="flex min-h-screen p-24">
-      {data?.map((d: { name: string }) => d.name)}
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr>
+              {headerGroup.headers.map((header) => (
+                <th>{header.renderHeader()}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr>
+              {row.getVisibleCells().map((cell) => (
+                <td>{cell.renderCell()}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </main>
   );
 }
+
+const COLOUMN = [
+  {
+    Header: "نام",
+    accessor: "name",
+    id: "name",
+  },
+  {
+    Header: "لقب",
+    accessor: "nickname",
+    id: "nickname",
+  },
+  {
+    Header: "تولد",
+    accessor: "birthYearInLHijri",
+    id: "birthYearInLHijri",
+  },
+  {
+    Header: "وفات",
+    accessor: "deathYearInLHijri",
+    id: "deathYearInLHijri",
+  },
+  {
+    Header: "محل تولد",
+    accessor: "birthPlace",
+    id: "birthPlace",
+  },
+  {
+    Header: "محل مرگ",
+    accessor: "deathPlace",
+    id: "deathPlace",
+  },
+  {
+    Header: "تصویر",
+    accessor: "imageUrl",
+    id: "imageUrl",
+  },
+];
